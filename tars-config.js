397f1c9af25fbd3fb111f6e50210380b546946ec
Юрий Чижов <ych@vlz.local>
2018-06-20 14:36:54 +0300

    update to 1.9.9

--- tars-config.js
+++ tars-config.js
@@ -1,63 +1,62 @@
 module.exports = {
-  postcss: [],
-  svg: {
-    active: true,
-    workflow: 'symbols',
-    symbolsConfig: {
-      loadingType: 'separate-file',
-      usePolyfillForExternalSymbols: true,
-      pathToExternalSymbolsFile: 'static/img',
+    "postcss": [],
+    "svg": {
+        "active": true,
+        "workflow": "symbols",
+        "symbolsConfig": {
+            "loadingType": "separate-file",
+            "usePolyfillForExternalSymbols": true,
+            "pathToExternalSymbolsFile": "static/img"
+        }
     },
-  },
-  css: {
-    workflow: 'manual',
-  },
-  js: {
-    workflow: 'modular',
-    bundler: 'webpack',
-    lint: true,
-    useBabel: true,
-    removeConsoleLog: true,
-    webpack: {
-      useHMR: false,
-      providePlugin: {},
+    "css": {
+        "workflow": "manual"
     },
-    jsPathsToConcatBeforeModulesJs: [],
-    lintJsCodeBeforeModules: false,
-    jsPathsToConcatAfterModulesJs: [],
-    lintJsCodeAfterModules: false,
-  },
-  sourcemaps: {
-    js: {
-      active: true,
-      inline: true,
+    "js": {
+        "workflow": "modular",
+        "bundler": "webpack",
+        "lint": true,
+        "useBabel": true,
+        "removeConsoleLog": true,
+        "webpack": {
+            "useHMR": false,
+            "providePlugin": {}
+        },
+        "jsPathsToConcatBeforeModulesJs": [],
+        "lintJsCodeBeforeModules": false,
+        "jsPathsToConcatAfterModulesJs": [],
+        "lintJsCodeAfterModules": false
     },
-    css: {
-      active: true,
-      inline: true,
+    "sourcemaps": {
+        "js": {
+            "active": true,
+            "inline": true
+        },
+        "css": {
+            "active": true,
+            "inline": true
+        }
     },
-  },
-  notifyConfig: {
-    useNotify: true,
-    title: 'TARS notification',
-    sounds: {},
-    taskFinishedText: 'Task finished at: ',
-  },
-  minifyHtml: false,
-  generateStaticPath: true,
-  buildPath: './build/',
-  useBuildVersioning: false,
-  useArchiver: false,
-  ulimit: 4096,
-  templater: 'pug',
-  cssPreprocessor: 'stylus',
-  useImagesForDisplayWithDpi: [
-    96,
-  ],
-  fs: {
-    staticFolderName: 'static',
-    imagesFolderName: 'img',
-    componentsFolderName: 'components',
-  },
-  staticPrefix: 'static/',
+    "notifyConfig": {
+        "useNotify": true,
+        "title": "TARS notification",
+        "sounds": {},
+        "taskFinishedText": "Task finished at: "
+    },
+    "minifyHtml": false,
+    "generateStaticPath": true,
+    "buildPath": "./build/",
+    "useBuildVersioning": false,
+    "useArchiver": false,
+    "ulimit": 4096,
+    "templater": "pug",
+    "cssPreprocessor": "stylus",
+    "useImagesForDisplayWithDpi": [
+        96
+    ],
+    "fs": {
+        "staticFolderName": "static",
+        "imagesFolderName": "img",
+        "componentsFolderName": "components"
+    }
 };
